6a44a4e91dbc59614e0621d2661af1bb75f9c3a5
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-03-21 15:45:46 +0100

    fixed

--- ejemplo_2_template/cuda_template.cu
+++ ejemplo_2_template/cuda_template.cu
@@ -72,8 +72,8 @@ int main(int argc, char **argv)
     tam_block_z = getCmdLineArgumentInt(argc, (const char **) argv, (const char *) "bsz") ? : tam_block_z;
 
 
-    printf("\n\nDimension malla: tam_grid_x=%d tam_grid_y=%d tam_grid_z=%d \n", tam_grid_x,tam_grid_y, tam_grid_z);  
-    printf("Dimension bloque: tam_block_x=%d tam_grid_y=%d\n\n",tam_block_x,tam_block_y);
+    printf("\n\nDimension malla: tam_grid_x=%d tam_grid_y=%d \n", tam_grid_x,tam_grid_y,);  
+    printf("Dimension bloque: tam_block_x=%d tam_block_y=%d tam_block_z=%d \n\n",tam_block_x, tam_block_y, tam_block_z);
 
 
     nPos = tam_grid_x * tam_grid_y * tam_block_x * tam_block_y * tam_block_z;
