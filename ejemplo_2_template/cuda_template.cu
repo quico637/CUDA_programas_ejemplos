a70502e950a61f020053f1be235eb3485ff6f290
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-03-21 10:10:27 +0100

    change printf

--- ejemplo_2_template/cuda_template.cu
+++ ejemplo_2_template/cuda_template.cu
@@ -72,7 +72,7 @@ int main(int argc, char **argv)
     tam_block_z = getCmdLineArgumentInt(argc, (const char **) argv, (const char *) "bsz") ? : tam_block_z;
 
 
-    printf("\n\nDimension malla: tam_grid_x=%d tam_grid_y=%d \n", tam_grid_x,tam_grid_y);  
+    printf("\n\nDimension malla: tam_grid_x=%d tam_grid_y=%d tam_grid_z=%d \n", tam_grid_x,tam_grid_y, tam_grid_z);  
     printf("Dimension bloque: tam_block_x=%d tam_grid_y=%d\n\n",tam_block_x,tam_block_y);
 
 
