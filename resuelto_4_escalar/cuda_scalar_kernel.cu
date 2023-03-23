13735e88cc72ff0fcaa7f499448f6550d2996029
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-03-23 19:05:52 +0100

    fix

--- resuelto_4_escalar/cuda_scalar_kernel.cu
+++ resuelto_4_escalar/cuda_scalar_kernel.cu
@@ -5,13 +5,13 @@
 __global__ void vectorScalarProduct(float *vector_d, float *wector_d, float *scalar_d, int n)
 {
     extern __shared__ float shared[];
-        
+    
     // global thread ID in grid
     int tidg = blockIdx.x * blockDim.x + threadIdx.x;
 
     
     if(tidg < n) {
-        shared[tidg] = v_shared[tidg] * w_shared[tidg];   
+        shared[tidg] = vector_d[tidg] * wector_d[tidg];   
         scalar_d[tidg] = shared[tidg];
     }
 
