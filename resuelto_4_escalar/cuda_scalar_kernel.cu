3594b779de2dae271085ee80d8b72a9890660806
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-03-23 19:03:51 +0100

    new approach

--- resuelto_4_escalar/cuda_scalar_kernel.cu
+++ resuelto_4_escalar/cuda_scalar_kernel.cu
@@ -4,28 +4,15 @@
 
 __global__ void vectorScalarProduct(float *vector_d, float *wector_d, float *scalar_d, int n)
 {
-    extern __shared__ float v_shared[];
-    extern __shared__ float w_shared[];
-    extern __shared__ float s_shared[];
-
-    // global thread ID in thread block
-    int tidb = threadIdx.x;
-    
+    extern __shared__ float shared[];
+        
     // global thread ID in grid
     int tidg = blockIdx.x * blockDim.x + threadIdx.x;
 
-
-	//printf("blockIdx.x=%d threadIdx.x=%d \n",blockIdx.x,threadIdx.x);
-
-    // load shared memory
-
-    v_shared[tidb] = (tidg < n) ? vector_d[tidg] : 1.0;
-    w_shared[tidb] = (tidg < n) ? wector_d[tidg] : 1.0;
-
-
+    
     if(tidg < n) {
-        s_shared[tidg] = v_shared[tidg] * w_shared[tidg];   
-        scalar_d[tidg] = s_shared[tidb];
+        shared[tidg] = v_shared[tidg] * w_shared[tidg];   
+        scalar_d[tidg] = shared[tidg];
     }
 
 
