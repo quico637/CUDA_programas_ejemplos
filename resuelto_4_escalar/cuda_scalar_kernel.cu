7f623cae618102c2a7e70bfadb691d2cb49dc123
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-03-23 18:46:33 +0100

    printf

--- resuelto_4_escalar/cuda_scalar_kernel.cu
+++ resuelto_4_escalar/cuda_scalar_kernel.cu
@@ -6,6 +6,8 @@ __global__ void vectorScalarProduct(float *vector_d, float *wector_d, float *sca
 {
     extern __shared__ int sdata[];
 
+    print("legué");
+
     // global thread ID in thread block
     unsigned int tidb = threadIdx.x;
     
