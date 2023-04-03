6c10bd96743c96edd901165c72940e0e90bf7406
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-04-03 17:24:16 +0200

    a ver el 3ero

--- proyecto/compara_kernels_kernel.cu
+++ proyecto/compara_kernels_kernel.cu
@@ -40,23 +40,23 @@ __global__ void coalescedMultiply(float *a, float *b, float *c, int N, const int
 /* Para calcular cada fila del tile de C se lee el tile entero de B. Por tanto, para el tile entero de C (el trabajo que hace un bloque de threads) se lee el tile entero de B 
 repetidamente (w veces) */
 
-// __global__ void sharedABMultiply(float *a, float *b, float *c, int N, int tile_dim)
-// {
-//     __shared__ float aTile[tile_dim][tile_dim],
+__global__ void sharedABMultiply(float *a, float *b, float *c, int N, const int tile_dim)
+{
+    __shared__ float aTile[],
 
-//     bTile[tile_dim][tile_dim];
-//     int row = blockIdx.y * blockDim.y + threadIdx.y;
-//     int col = blockIdx.x * blockDim.x + threadIdx.x;
-//     float sum = 0.0f;
+    bTile[tile_dim][tile_dim];
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    float sum = 0.0f;
 
-//     aTile[threadIdx.y][threadIdx.x] = a[row * tile_dim + threadIdx.x];
-//     bTile[threadIdx.y][threadIdx.x] = b[threadIdx.y * N + col];
+    aTile[threadIdx.y * tile_dim + threadIdx.x] = a[row * tile_dim + threadIdx.x];
+    bTile[threadIdx.y][threadIdx.x] = b[threadIdx.y * N + col];
 
-//     __syncthreads(); // warp usa datos de B leídos por otro warp
+    __syncthreads(); // warp usa datos de B leídos por otro warp
 
-//     for (int i = 0; i < tile_dim; i++)
-//     {
-//         sum += aTile[threadIdx.y][i] * bTile[i][threadIdx.x];
-//     }
-//     c[row * N + col] = sum;
-// }
+    for (int i = 0; i < tile_dim; i++)
+    {
+        sum += aTile[threadIdx.y * tile_dim + i] * bTile[i][threadIdx.x];
+    }
+    c[row * N + col] = sum;
+}
