cb72d47944e2838da730fe3e6805d773b31e5e8e
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-04-03 16:37:37 +0200

    s

---
+++ proyecto/compara_kernels_kernel.cu
@@ -0,0 +1,49 @@
+////////////////////////////////////////////////////////////////////////////////
+// vectorReduce kernel
+////////////////////////////////////////////////////////////////////////////////
+
+__global__ void vectorReduce(float *vector_d, float *reduce_d, int n)
+{
+    extern __shared__ int sdata[];
+
+    // global thread ID in thread block
+    unsigned int tidb = threadIdx.x;
+
+    // global thread ID in grid
+    unsigned int tidg = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // printf("blockIdx.x=%d threadIdx.x=%d \n",blockIdx.x,threadIdx.x);
+
+    // load shared memory
+    sdata[tidb] = (tidg < n) ? vector_d[tidg] : 0;
+
+    // __syncthreads();
+
+    if (blockDim.x % 2 != 0 && blockDim.x > 1 && tidb == 0)
+    {
+        atomicAdd(&sdata[0], sdata[blockDim.x - 1]);
+    }
+
+    // perform reduction in shared memory
+    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
+    {
+
+        if (tidb < s)
+        {
+            sdata[tidb] += sdata[tidb + s];
+        }
+
+        if (s % 2 != 0 && s > 1 && tidb == 0)
+        {
+            atomicAdd(&sdata[0], sdata[s - 1]);
+        }
+
+        // __syncthreads();
+    }
+
+    // write result for this block to global memory
+    if (tidb == 0)
+    {
+        reduce_d[blockIdx.x] = sdata[0];
+    }
+}
