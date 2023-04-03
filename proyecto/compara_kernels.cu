331f302c882b4d82d462b8537f427794258c89e7
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-04-03 16:42:55 +0200

    a ver si con 32 W

--- proyecto/compara_kernels.cu
+++ proyecto/compara_kernels.cu
@@ -27,7 +27,7 @@
 #include "compara_kernels_kernel.cu"
 
 #define TEST
-#define DEBUG
+// #define DEBUG
 
 ////////////////////////////////////////////////////////////////////////////////
 // Program main
@@ -58,7 +58,7 @@ float * multiply(float *A, float *B,  float *res, int m, int n, int w)
                 C[i * n + j] += A[i * w + k] * B[k * n + j];
             }
 
-            // assert(C[i * n + j] == res[i * n + j]);
+            assert(C[i * n + j] == res[i * n + j]);
             
         }
     }
