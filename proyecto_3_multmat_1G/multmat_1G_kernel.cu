50435990ad0ca4e9cbe1ccc176e9f733602d2ad0
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-04-17 18:40:35 +0200

    a ver el 3

--- proyecto_3_multmat_1G/multmat_1G_kernel.cu
+++ proyecto_3_multmat_1G/multmat_1G_kernel.cu
@@ -1,23 +1,32 @@
 /* Para calcular cada fila del tile de C se lee el tile entero de B. Por tanto, para el tile entero de C (el trabajo que hace un bloque de threads) se lee el tile entero de B
 repetidamente (w veces) */
 
-__global__ void sharedABMultiply(const float *a, const float *b, float *c, const int N, const int tile_dim)
+__global__ void sharedABMultiply(float *a, float *b, float *c, int N, const int tile_dim)
 {
     extern __shared__ float aTile[];
 
-    float *bTile = aTile + (tile_dim * tile_dim);
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
     float sum = 0.0f;
 
-    aTile[threadIdx.y * tile_dim + threadIdx.x] = a[row * tile_dim + threadIdx.x];
-    bTile[threadIdx.y * tile_dim + threadIdx.x] = b[threadIdx.y * N + col];
-
-    __syncthreads(); // warp usa datos de B leídos por otro warp
-
-    for (int i = 0; i < tile_dim; i++)
+    for (int tileIdx = 0; tileIdx < N / tile_dim; tileIdx++)
     {
-        sum += aTile[threadIdx.y * tile_dim + i] * bTile[i * tile_dim + threadIdx.x];
+        float *bTile = aTile + (tile_dim * tile_dim);
+        int row = blockIdx.y * blockDim.y + threadIdx.y;
+        int col = tileIdx * blockDim.x + threadIdx.x;
+        
+
+        aTile[threadIdx.y * tile_dim + threadIdx.x] = a[row * N + threadIdx.x];
+        bTile[threadIdx.y * tile_dim + threadIdx.x] = b[threadIdx.y * N + col];
+
+        __syncthreads(); // warp usa datos de B leídos por otro warp
+
+        for (int i = 0; i < tile_dim; i++)
+        {
+            sum += aTile[threadIdx.y * tile_dim + i] * bTile[i * tile_dim + threadIdx.x];
+        }
     }
+
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
     c[row * N + col] = sum;
 }
