91350649aba38b8eb5ee90512487cfbf2c879b0d
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-04-25 02:23:59 +0200

    script update

--- proyecto/script.py
+++ proyecto/script.py
@@ -13,7 +13,7 @@ DIM_MAT = [512, 1024, 2048, 4096]
 TAM_BLO = [4, 8, 16, 32]
 KERNELS = [1, 2, 3]
 
-os.system("echo \"Kernel;Dim mat;Block Size;Time\" >> " + FILE)
+os.system("echo \"Kernel;Dim mat;Block Size;Time (ms)\" >> " + FILE)
 
 for k in KERNELS:
     for i in DIM_MAT:
