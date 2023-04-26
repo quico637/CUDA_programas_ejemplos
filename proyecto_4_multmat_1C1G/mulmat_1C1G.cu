2150676301fd61c21b1e997bff263bdf72bc8453
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-04-26 02:56:28 +0200

    a

--- proyecto_4_multmat_1C1G/mulmat_1C1G.cu
+++ proyecto_4_multmat_1C1G/mulmat_1C1G.cu
@@ -44,7 +44,7 @@ float *multiply_row(float *A, float *B, float *C, int m, int n, int w, int row)
 
     int i, j, k;
 
-#pragma omp for private(i, j, k) schedule(dynamic, SCHEDULE_RATIO)
+#pragma omp for private(i, j, k)
     for (i = row; i < m; i++)
     {
         for (j = 0; j < n; j++)
